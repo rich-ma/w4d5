0bf358998f82cfac8eb4d2951c797a4674d5ba23
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-13 13:42:45 -0700

    Add User model and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login_user(@user)
+      redirect_to users_url(@user)
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_user_url
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+end
